3ec494d5d3b48277cf410a96db13cd913886a38d
xpc <pengcheng.xiang@daddylab.com>
2022-03-07 17:26:03 +0800

    全排列

--- LC/array/medium/medium_test.go
+++ LC/array/medium/medium_test.go
@@ -1,7 +1,15 @@
 package medium
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestSingleNumber(t *testing.T){
 	singleNumber([]int{2,2,3,2})
+}
+
+func TestPermute(t *testing.T){
+	ret:=permute([]int{1,2,3})
+	fmt.Println(ret)
 }
